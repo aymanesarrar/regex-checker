b98a0e8a5978fff939dd6973097f8792691432db
aymanesarrar <aymensarrar1@gmail.com>
2025-04-24 23:55:39 +0100

    add an is_valid_pattern function to check for a valid patern and add support for dot character

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,33 @@ fn main() {
     println!("{}", result)
 }
 
+fn is_valid_pattern(pattern: &str) -> bool {
+    // collect chars
+    let chars: Vec<char> = pattern.chars().collect();
+    if chars.is_empty() {
+        return true
+    }
+    for i in 0..chars.len() {
+        if chars[i] == '+' || chars[i] == '*' {
+            if i == 0 || chars[i - 1] == '+' || chars[i - 1] == '*' {
+                return false
+            }
+        } 
+    }
+    return true;
+}
+
 fn regex_checker(pattern: &String, str: &String) -> bool {
     if pattern.len() != str.len() {
         return false;
     }
-    return false;
+    if !is_valid_pattern(pattern) {
+        return false
+    }
+    for (p_char, str_char) in pattern.chars().zip(str.chars()) {
+        if p_char != str_char && p_char != '.' {
+            return false;
+        }
+    }
+    return true;
 }
